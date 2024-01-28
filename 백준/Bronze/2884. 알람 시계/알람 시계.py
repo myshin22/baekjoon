a673216afb56426d3f3f8554c1d6bee5d913d083
myshin22 <127702569+myshin22@users.noreply.github.com>
2024-01-29 01:15:03 +0900

    [Bronze III] Title: 알람 시계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -0,0 +1,14 @@
+H,M = map(int,input().split())
+
+if M >= 45:
+    H = H
+    M = M -45
+    print(H, M)
+    
+elif M <45:
+    H = H -1
+    M = M +60 -45
+    if H < 0:
+        H = 23
+    print(H, M)
+
