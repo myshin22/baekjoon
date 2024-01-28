bcdc0b8ca6a5cf7569a65012c18160f336361ac3
myshin22 <127702569+myshin22@users.noreply.github.com>
2024-01-29 01:15:16 +0900

    [Bronze III] Title: 오븐 시계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -0,0 +1,28 @@
+A, B = map(int, input().split())
+C = int(input())
+
+if C < 60:
+    B = B + C
+    if B >= 60:
+        A = A+1
+        B = B-60
+    if A >=24:
+        A = A-24
+    elif A<0:
+        A = A+24
+
+    print(A,B)
+
+else:
+    B = B +  C % 60
+    A = A + C//60
+    if B >= 60:
+        A = A+1
+        B = B-60
+    if A >=24:
+        A = A-24
+    elif A<0:
+        A = A+24
+
+    print(A,B)
+
